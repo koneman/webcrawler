212ef375f826352dd1c323116f0c4a59614402b3
Sathvik Koneru <sathvik@koneru.org>
2017-06-21 18:45:25 -0700

    fixing bugs in address Loc

--- addressNLP.py
+++ addressNLP.py
@@ -10,9 +10,23 @@ import re
 
 #getting all text on a page
 def getHTMLtext (url):
-    page = urllib.urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
     HTMLtext = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in HTMLtext.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    HTMLtext = '\n'.join(chunk for chunk in chunks if chunk)
+
     return HTMLtext
 
 def findConsecNouns(inputText):
@@ -22,29 +36,24 @@ def findConsecNouns(inputText):
 
         POStagged = nltk.pos_tag(tokenizedText)
 
-        #print(POStagged)
-        #get all nouns
+        #accepted nltk POS tags - NN, NNP, nouns
         wordLength = range(len(POStagged)-1)
         nounList = ''
+        word = r'[A-Za-z]+\s'
+        firstPart = r'\s'
 
         for x in wordLength:
-            #word = POStagged[x]
-            #consecutive_word = POStagged[x+1]
-            if POStagged[x][1] == POStagged[x+1][1]:
+            if (POStagged[x][1] and POStagged[x+1][1]) in ['NN','NNP','NNS']:
                 nounList += POStagged[x][0] + POStagged[x+1][0]
                 #print "Noun: ", POStagged[x][0], POStagged[x+1][0]
-        #print nounList
+        print nounList
 
 
 def nltkPractice (webText):
 
-    streetNumber = r'\d+'
+    streetNumber = r'\d+ '
     #address = re.compile(streetNumber)
     address = re.findall(streetNumber, webText)
-    numberList = []
-
-    for number in address:
-        numberList.append(number)
 
     #for number in address.finditer(webText):
     #    location.append(number.end())
@@ -54,14 +63,17 @@ def nltkPractice (webText):
     print neededText
     """
 
-    for number in numberList:
-        numberEndLoc = webText.find(number) + len(number) + 1
+    for number in address:
+        numberEndLoc = webText.find(number) + len(number)
         neededText = webText[numberEndLoc:]
+        print number
+        findConsecNouns(neededText)
         #print neededText
 
 
 url = 'https://achildshopefoundation.org'
 HTMLtext = getHTMLtext(url)
+#print HTMLtext
 #GetAddresses(url)
 
 
