478525e13a193e9280dbd71a55131f41486bfde4
Sathvik Koneru <sathvik@koneru.org>
2017-07-02 10:49:41 -0700

    google API

--- googleNLP.py
+++ googleNLP.py
@@ -10,3 +10,5 @@ def implicit():
     # Make an authenticated API request
     buckets = list(storage_client.list_buckets())
     print(buckets)
+
+gcloud beta ml language analyze-entities --content="Contact Us iconSend us a messageicon801.434.9200iconinfo@achildshopefoundation.orgiconOffice: 165 North 1330 West Ste. A1, Orem Utah 84057iconMailing: 2727 West 620 North, Provo Utah 84601"
