97cbb35ed75f3e2c40fedc74e8c0b3c121cd600c
Sathvik Koneru <sathvik@koneru.org>
2017-07-03 12:13:26 -0700

    finalizing alg

--- addressAlg.py
+++ addressAlg.py
@@ -34,29 +34,29 @@ def getAddress(webText):
 
 
         streetNumber = r'\d+'
-        stateAbbrev = r'(AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT \
-                |NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY)?'
-        state = r'(Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii| \
+        state = r'(AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT \
+                |NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY \
+                Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii| \
                 Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|\
                 Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New[ ]Hampshire|New[ ]Jersey|New[ ]Mexico|\
                 New[ ]York|North[ ]Carolina|North[ ]Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode[ ]Island|\
-                South[ ]Carolina|South[ ]Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West[ ]Virginia|Wisconsin|Wyoming)?'
+                South[ ]Carolina|South[ ]Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West[ ]Virginia|Wisconsin|Wyoming)'
         zipCode = r'[ ]+(\b\d{5}(?:-\d{4})?\b)'
 
 
-        addressBenchmark = stateAbbrev + state + zipCode
+        addressBenchmark = state + zipCode
 
         #find address
         address = re.findall(addressBenchmark, webText)
+        print address
 
         stringReform = ''
         index = 0
         addressList = ['','']
         #append string
-        for (a,b,c) in address:
-            stringReform = a + b + ' ' + c
+        for (a,b) in address:
+            stringReform = a + ' ' + b
             addressList[index] = re.sub("^u'(.*)'$",r'\1',stringReform)
-            streetNum = c
             index += 1
 
         #print addresses
@@ -64,21 +64,36 @@ def getAddress(webText):
 
         #find position of zipcode
         for x in addressList:
-            numberEndLoc = webText.find(streetNum) + len(x) + 1
-            print numberEndLoc
+            numberEndLoc = webText.find(x) + len(x) + 1
 
             #print (webText.find(streetNum))
 
             #assign starting point for looking for street number
-            startSearch = numberEndLoc - 25
-            neededText = webText[startSearch:]
+            startSearch = numberEndLoc - 55
+
+            neededText = webText[startSearch:numberEndLoc]
+
             #print neededText
             street_number = re.findall(streetNumber,neededText)
 
             print street_number
 
-            addressStart = webText.find(streetNumber,street_number)
-            print(webText[addressStart:numberEndLoc])
+            addressStart = []
+            for s in street_number:
+                addressStart.append(neededText.find(s))
+
+            print addressStart
+            print neededText[addressStart[0]:-1]
+
+            addressData = []
+            addFinal = ''
+            for a in addressStart:
+                addFinal = neededText[a:]
+                print(addFinal)
+                addressData.append(addFinal)
+
+        print addressData
+
 
 
 url = 'https://achildshopefoundation.org/endeavors/haiti'
