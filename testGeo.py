26d74bdf9715ab94aad78b7bc9b0267427659642
Sathvik Koneru <sathvik@koneru.org>
2017-07-06 12:10:36 -0700

    Delete testGeo.py

--- testGeo.py
+++
@@ -1,8 +0,0 @@
-import geograpy
-
-
-e = extraction.Extractor(url='http://www.bbc.com/news/world-europe-26919928')
-e.find_entities()
-
-# You can now access all of the places found by the Extractor
-print e.places
